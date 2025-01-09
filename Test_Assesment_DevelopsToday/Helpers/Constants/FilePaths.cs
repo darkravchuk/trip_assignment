5270c7e1f4d73d1c16e4c68d09dd7e91d3477adb
Daria Kravchuk <daria1kravchuk@gmail.com>
2025-01-09 08:45:20 +0200

    chore: update file paths and configuration in project file

--- Test_Assesment_DevelopsToday/Helpers/Constants/FilePaths.cs
+++ Test_Assesment_DevelopsToday/Helpers/Constants/FilePaths.cs
@@ -3,5 +3,5 @@ namespace Test_Assesment_DevelopsToday.Helpers.Constants;
 public static class FilePaths
 {
     public const string SampleCabDataFileName = "sample-cab-data.csv";
-    public const string DuplicateDataFileName = "sample-cab-data.csv";
+    public const string DuplicateDataFileName = "duplicates.csv";
 }
